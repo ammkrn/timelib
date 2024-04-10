f31c615728ee61b8698e896d07818b8ef416eb47
Adrien Champion <adrien.champion@protonmail.com>
2024-04-10 15:05:08 +0200

    chore: add optional docgen dependency, discuss in readme

--- lakefile.lean
+++ lakefile.lean
@@ -11,5 +11,8 @@ lean_lib Timelib
 require mathlib from git
   "https://github.com/leanprover-community/mathlib4.git"@"stable"
 
+meta if get_config? env = some "dev" then -- dev is so not everyone has to build it
+require «doc-gen4» from git "https://github.com/leanprover/doc-gen4" @ "main"
+
 -- @ 58f072
 --require mathlib from ".."/"Mathlib4"
